a9c77a91415796e1fe2324f05c0aa5f30b3574ec
Edwin Perez <edwinperez@edwins-mbp.home>
2019-07-08 10:35:35 -0700

    Small changes

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get("/", (req,res) => {
 	res.send("Hello");
 });
 
-app.listen(3000, function(){
-	console.log("Servers Up");
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+	console.log("Website is up!");
 });
